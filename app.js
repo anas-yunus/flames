766cd1f3cc166902272cb55392c4f5c244de7110
Anas Yunus <anasyunusind@gmail.com>
2022-10-02 00:03:06 +0530

    Finishing up

--- app.js
+++ app.js
@@ -2,17 +2,24 @@
 const boyInputEl = document.getElementById("boy")
 const girlInputEl = document.getElementById("girl")
 const outputEl = document.getElementById("output")
-
+const loggerEl = document.getElementById("logg")
+let flamesArray = ["F", "L", "A", "M", "E", "S"]
 
 function execute() {
 
     //INITIALIZATION
     let flamesArray = ["F", "L", "A", "M", "E", "S"]
     let difference=''
-    let boyName = boyInputEl.value
-    let girlName = girlInputEl.value
+    let boyName = boyInputEl.value.toLowerCase()
+    let girlName = girlInputEl.value.toLowerCase()
+    if(boyName == "" || girlName == ""){
+        
+    }
+    boyName = boyName.replace(/\s/g, "")
+    girlName = girlName.replace(/\s/g, "")
     boyNameArray = boyName.split("")
     girlNameArray = girlName.split("")
+    console.log(boyNameArray,girlNameArray);
 
     // CHARACTER SPLITTING
     const x = boyNameArray.filter(x => !girlNameArray.includes(x));
@@ -21,11 +28,18 @@ function execute() {
 
     //CUTTING FLAMES
     console.log("difference ", difference.length);
+    //loggerEl.innerHTML = `Difference ${difference.length} <br>`
+    let lastIndex = 0;
     for (let i = 0; i < 5; i++) {
         console.log(flamesArray)
-        const i = difference.length //index you wanna access
+        //loggerEl.innerHTML += `${flamesArray} <br>`
+        const i = difference.length - 1 //index you wanna access
         const n = flamesArray.length
-        flamesArray.splice((i % n + n) % n, 1)
+        //console.log();
+        let x = ((i + lastIndex ) % n + n) % n
+        flamesArray.splice(x, 1)
+        lastIndex = x
+        //console.log(lastIndex)
     }
 
     //RESULT CARRYING AND OUTPUT
